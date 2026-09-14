116bd15ff61caf9a071f1237ee57f285fa12f295
ArielCampos30 <153675296+ArielCampos30@users.noreply.github.com>
2026-09-14 20:01:50 -0300

    Bloque 8.8J: retirar Chat de navegación visible del empleado

--- web-amc/public/app-shell-navigation.js
+++ web-amc/public/app-shell-navigation.js
@@ -1,5 +1,5 @@
 const ADMIN_LINKS=[['inicio','Inicio','⌂'],['solicitudes','Solicitudes','▤'],['presupuestos','Presupuestos','▤'],['obras','Obras','⌂'],['mas-admin','Más','•••']];
-const EMPLOYEE_LINKS=[['inicio-empleado','Inicio','⌂'],['mis-trabajos','Mis trabajos','▦'],['chat-equipo','Chat','◌'],['perfil','Perfil','○']];
+const EMPLOYEE_LINKS=[['inicio-empleado','Inicio','⌂'],['mis-trabajos','Mis trabajos','▦'],['perfil','Perfil','○']];
 const CLIENT_LINKS=[['inicio','Inicio','⌂'],['mis-trabajos-cliente','Mis trabajos','▦'],['chat-cliente','Chat','◌'],['perfil','Perfil','○']];
 const PUBLIC_SIDEBAR_LINKS=[['inicio','Inicio','⌂'],['servicios','Servicios','▦'],['ideas','Ideas para tu casa','✧'],['favoritos','Guardados','♡'],['presupuestos','Presupuestos','▤'],['obra','Mi obra','⌂'],['resenas','Reseñas','☆'],['perfil','Mi perfil','○'],['mensajes','Mensajes','◌'],['agenda','Agenda','▦'],['adicionales','Adicionales','＋'],['comprobantes','Comprobantes','▤'],['compras','Compras y facturas','▤']];
 const PUBLIC_BOTTOM_LINKS=[['inicio','Inicio','⌂'],['servicios','Servicios','▦'],['pedir','Pedir','＋'],['obra','Mi obra','▤'],['perfil','Perfil','○']];
