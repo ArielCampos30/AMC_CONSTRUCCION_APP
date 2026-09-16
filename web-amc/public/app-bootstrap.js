a58d1db9705aa9a245e05a86adbf90709b38064d
ArielCampos30 <153675296+ArielCampos30@users.noreply.github.com>
2026-09-16 12:32:31 -0300

    Bloque 12.10B: restaurar append nativo tras bootstrap

--- web-amc/public/app-bootstrap.js
+++ web-amc/public/app-bootstrap.js
@@ -1,6 +1,6 @@
-const head=document.head,nativeAppend=head.append;
+const head=document.head,nativeAppend=head.append,hadOwnAppend=Object.prototype.hasOwnProperty.call(head,'append');
 head.append=function(...nodes){
  const allowed=nodes.filter(node=>!(node?.tagName==='LINK'&&node.getAttribute?.('href')==='/admin-v3.css'&&!node.dataset?.amcRoleStyle));
  if(allowed.length)return nativeAppend.apply(this,allowed);
 };
-try{await import('./app.js');}finally{head.append=nativeAppend;}
+try{await import('./app.js');}finally{if(hadOwnAppend)head.append=nativeAppend;else delete head.append;}
